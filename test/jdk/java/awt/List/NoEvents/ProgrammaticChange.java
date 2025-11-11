1cd9ebade54da52fff98757000b26863035cbce7
Sergey Bylokhov <bylokhov@amazon.com>
2025-11-11 14:08:45 -0800

    add bugid: 8371657

--- test/jdk/java/awt/List/NoEvents/ProgrammaticChange.java
+++ test/jdk/java/awt/List/NoEvents/ProgrammaticChange.java
@@ -29,7 +29,7 @@ import java.util.function.Supplier;
 
 /**
  * @test
- * @bug 1234567
+ * @bug 8371657
  * @key headful
  * @summary Checks that programmatic changes to a List do not fire events
  */
